1ae9a4ae5a6c7857b31e0a7b72a73d31fa0708e7
bitsofcotton <kazunobu@mail.limpid-intensity.info>
2018-06-13 21:32:36 +0900

    test.py enl revert.

--- test.py
+++ test.py
@@ -79,7 +79,7 @@ for line in argv[3:]:
     s0 = int(np.ceil(np.log(pixels) / np.log(2.)))
     for s in range(0, s0):
       subprocess.call([argv[1], "enlarge", root + "-" + str(s) + ".ppm", root + "-" + str(s + 1) + "-base.ppm"])
-      subprocess.call(["convert", root + "-" + str(s + 1) + "-base.ppm", "-blur", "0x1", "-sharpen", "0x1", "-compress", "none", root + "-" + str(s + 1) + ".ppm"])
+      subprocess.call(["convert", root + "-" + str(s + 1) + "-base.ppm", "-blur", "0x1", "-sharpen", "0x" + str(pow(2, s)), "-compress", "none", root + "-" + str(s + 1) + ".ppm"])
   elif(argv[2] == "habit"):
     if(len(bhabit) <= 0):
       bhabit = line
