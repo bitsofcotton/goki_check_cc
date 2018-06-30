9f865cda24f3cafea8e46b345ef792158450c5ec
bitsofcotton <kazunobu@mail.limpid-intensity.info>
2018-06-30 12:21:57 +0900

    fixed direction sign mismatch.

--- redig.hh
+++ redig.hh
@@ -852,7 +852,7 @@ template <typename T> triangles_t<T> reDig<T>::makeTriangle(const int& u, const 
   }
   work.c = T(0);
   for(int i = 0; i < 3;  i ++) {
-    work.p(2, i) = bump(int(work.p(0, i)), int(work.p(1, i))) * sqrt(T(bump.rows() * bump.cols())) * rz;
+    work.p(2, i) = - bump(int(work.p(0, i)), int(work.p(1, i))) * sqrt(T(bump.rows() * bump.cols())) * rz;
     work.c      += in(int(work.p(0, i)), int(work.p(1, i)));
   }
   work.c /= T(3);
