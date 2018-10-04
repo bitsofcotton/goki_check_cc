eb3b5c254cac16505e54d306a59bd49ec64f1673
bitsofcotton <kazunobu@mail.limpid-intensity.info>
2018-10-04 20:57:01 +0900

    fix match offset glitch.

--- match.hh
+++ match.hh
@@ -412,6 +412,13 @@ template <typename T> bool matchPartial<T>::complementMatch(match_t<T>& work, co
   }
   work.offset *= num / denom;
   work.ratio  *= num / denom;
+  offset      *= T(0);
+  for(int k = 0; k < work.dstpoints.size(); k ++) {
+    const auto& shapek(shapebase[work.dstpoints[k]]);
+    const auto  pointk(work.transform(points[work.srcpoints[k]]));
+    offset += shapek - pointk;
+  }
+  work.offset = offset / work.dstpoints.size();
   for(int k = 0; k < work.dstpoints.size(); k ++) {
     const auto err(shapebase[work.dstpoints[k]] - work.transform(points[work.srcpoints[k]]));
     work.rdepth += err.dot(err);
