71fe633dd131366293be9a1b7f002ad32a6043c7
bitsofcotton <22259589+bitsofcotton@users.noreply.github.com>
2020-05-01 20:33:36 +0900

    test2 apply x instead of y.

--- test2.py
+++ test2.py
@@ -6,23 +6,23 @@ block = 44100
 def light(d, r):
   dr = []
   di = []
-  for uu in d:
-    dr.append(uu.real)
-    di.append(uu.imag)
+  for u in d:
+    dr.append(u.real)
+    di.append(u.imag)
   dr = np.fft.fft(dr)
   di = np.fft.fft(di)
-  for uu in range(1, len(dr)):
-    dr[uu] /= pow(np.exp(np.pi * 1.j * uu / len(dr)) - 1., r)
-    di[uu] /= pow(np.exp(np.pi * 1.j * uu / len(di)) - 1., r)
+  for u in range(1, len(dr)):
+    dr[u] /= pow(np.exp(np.pi * 1.j * u / len(dr)) - 1., r)
+    di[u] /= pow(np.exp(np.pi * 1.j * u / len(di)) - 1., r)
   dr = np.fft.ifft(dr)
   di = np.fft.ifft(di)
-  for uu in range(0, len(d)):
-    d[uu] = dr[uu].real + 1.j * di[uu].real
+  for u in range(0, len(d)):
+    d[u] = dr[u].real + 1.j * di[u].real
   return d
 
 def diff(d, r):
-  for uu in range(0, len(d)):
-    d[uu] *= pow(- 2.j * np.pi * uu / len(d), r)
+  for u in range(0, len(d)):
+    d[u] *= pow(- 2.j * np.pi * u / len(d), r)
   return d
 
 data = []
@@ -46,20 +46,26 @@ for v in range(0, int(sys.argv[2])):
     if(len(u) == 0):
       continue
     out.append([])
+    d2 = []
     for s in range(0, len(u) / block + 1):
-      d = np.fft.fft(u[s * block:min((s + 1) * block, len(u))])
+      d2.append(np.fft.fft(u[s * block:min((s + 1) * block, len(u))]))
+    for s in range(0, len(d2[0])):
+      d = []
+      for v in range(0, len(d2)):
+        if(s < len(d2[v])):
+          d.append(d2[v][s])
       if(sys.argv[1] == "ld"):
         d = diff(light(d, 1), 1)
       elif(sys.argv[1] == "l"):
         d = light(d, 1)
-      elif(sys.argv[1] == "L"):
-        d = light(u[s * block:min((s + 1) * block, len(u))], 1)
       elif(sys.argv[1] == "d"):
         d = diff(d, 1)
-      if(sys.argv[1] != "L"):
-        d = np.fft.ifft(d)
+      for v in range(0, len(d)):
+        d2[v][s] = d[v]
+    for s in range(0, len(u) / block + 1):
+      d = np.fft.ifft(d2[s])
       for v in d:
-        out[- 1].append(v.real)
+        out[- 1].append(float(v.real))
     MM = max(MM, sorted(out[- 1])[len(out[- 1]) * 7 / 8] * 2.)
   data = out
   for u in data:
