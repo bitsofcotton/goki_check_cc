40eaad8ad58660eb6cc2e16cd12386f0887a8059
bitsofcotton <22259589+bitsofcotton@users.noreply.github.com>
2020-09-29 23:16:02 +0900

    bump_y revert to correct algorithm.

--- enlarge.hh
+++ enlarge.hh
@@ -417,7 +417,7 @@ template <typename T> typename Filter<T>::Mat Filter<T>::compute(const Mat& data
 #endif
         for(int i = 0; i < A.rows(); i ++) {
           for(int j = 0; j < A.cols(); j ++)
-            if(zscore(i, j) < T(0) || abs(A(i, j)) <= zscore(i, j)) {
+            if(zscore(i, j) < abs(A(i, j))) {
               result(i, j) = T(zi) / T(dratio);
               zscore(i, j) = abs(A(i, j));
             }
