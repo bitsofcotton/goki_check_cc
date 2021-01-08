828db36a3395bb11a8bcbe1a1a47a36c3e9a0c7c
bitsofcotton <22259589+bitsofcotton@users.noreply.github.com>
2021-01-08 22:16:35 +0900

    merge latest fix on decompose

--- decompose.hh
+++ decompose.hh
@@ -38,13 +38,13 @@ public:
   inline Decompose();
   inline Decompose(const int& size);
   inline ~Decompose();
-         Vec mimic(const Vec& dst, const Vec& src, const T& intensity = T(1)) const;
-         Vec emphasis(const Vec& dst, const T& intensity = T(1)) const;
-         Vec next(const Vec& in) const;
-         Mat complementMat(const Vec& in) const;
+         Vec  mimic(const Vec& dst, const Vec& src, const T& intensity = T(1)) const;
+         Vec  emphasis(const Vec& dst, const T& intensity = T(1)) const;
+         Vec  mother(const Vec& in) const;
+         Vec  freq(const Vec& mother, const Vec& in) const;
+         Vec  synth(const Vec& mother, const Vec& freq) const;
 private:
-  std::vector<Mat> bA;
-  Mat A;
+  std::vector<Mat> A;
          Vec  prepare(const Vec& in, const int& idx = 0) const;
          void apply(Vec& v, const Vec& dst, const Vec& src, const int& idx = 0) const;
 };
@@ -55,18 +55,14 @@ template <typename T> inline Decompose<T>::Decompose() {
 
 template <typename T> inline Decompose<T>::Decompose(const int& size) {
   static P0<T> p0;
-  A.resize(size, size);
-  for(int i = 0; i < A.rows(); i ++)
-    for(int j = 0; j < A.cols(); j ++)
-      A(i, j) = T(0);
   for(int i = 0; i < size; i ++) {
     SimpleMatrix<T> AA(size, size);
     for(int j = 0; j < AA.rows(); j ++) {
-      const auto jj(T(j) * T(i + 1) / T(size));
+      // XXX: we avoid const. function.
+      const auto jj(T(j) * T(i + 1) / T(size + 1));
       AA.row(j) = p0.taylor(AA.cols(), (jj - floor(jj)) * T(size - 1));
     }
-    A += AA;
-    bA.emplace_back(AA);
+    A.emplace_back(AA);
   }
 }
 
@@ -75,36 +71,36 @@ template <typename T> inline Decompose<T>::~Decompose() {
 }
 
 template <typename T> typename Decompose<T>::Vec Decompose<T>::mimic(const Vec& dst, const Vec& src, const T& intensity) const {
-  const int  size(bA.size());
+  const int  size(A.size());
   const auto size2(dst.size() / size);
   const auto size3(src.size() / size);
         auto res(dst);
   for(int i = 0; i < size2; i ++) {
     const auto dd(prepare(dst, i));
-    apply(res, complementMat(next(prepare(src, i * size3 / size2))) *
-                 complementMat(next(dd)).solve(dd) * intensity +
+    apply(res, synth(mother(prepare(src, i * size3 / size2)),
+                     freq(mother(dd), dd)) * intensity +
                  dd * (T(1) - abs(intensity)), dd, i);
   }
   return res;
 }
 
 template <typename T> typename Decompose<T>::Vec Decompose<T>::emphasis(const Vec& dst, const T& intensity) const {
-  const int  size(bA.size());
+  const int  size(A.size());
   const auto size2(dst.size() / size);
         auto res(dst);
   for(int i = 0; i < size2; i ++) {
     const auto dd(prepare(dst, i));
-          auto freq(dd);
-    freq[freq.size() - 1] = T(0);
-    for(int j = 0; j < freq.size() - 1; j ++)
-      freq[j] = T(j + 1) / T(freq.size() - 1);
-    apply(res, complementMat(next(dd)) * freq * intensity, dd, i);
+          auto lfreq(dd);
+    lfreq[lfreq.size() - 1] = T(0);
+    for(int j = 0; j < lfreq.size() - 1; j ++)
+      lfreq[j] = T(j + 1) / T(lfreq.size() - 1);
+    apply(res, synth(mother(dd), lfreq) * intensity, dd, i);
   }
   return res;
 }
 
 template <typename T> typename Decompose<T>::Vec Decompose<T>::prepare(const Vec& in, const int& idx) const {
-  const int  size(bA.size());
+  const int  size(A.size());
   const auto cnt(int(in.size() + size - 1) / size - 1);
   assert(0 < cnt);
   Vec res(size);
@@ -125,7 +121,7 @@ template <typename T> typename Decompose<T>::Vec Decompose<T>::prepare(const Vec
 }
 
 template <typename T> void Decompose<T>::apply(Vec& v, const Vec& dst, const Vec& src, const int& idx) const {
-  const int  size(bA.size());
+  const int  size(A.size());
   assert(dst.size() == size && src.size() == size);
   const auto cnt(int(v.size() + size - 1) / size - 1);
   assert(0 < cnt);
@@ -143,21 +139,37 @@ template <typename T> void Decompose<T>::apply(Vec& v, const Vec& dst, const Vec
   return;
 }
 
-template <typename T> typename Decompose<T>::Vec Decompose<T>::next(const Vec& in) const {
-  assert(A.rows() == in.size());
-  auto f(A.solve(in));
+template <typename T> typename Decompose<T>::Vec Decompose<T>::mother(const Vec& in) const {
+  assert(in.size() && A.size() == in.size() &&
+         A[0].rows() == in.size() && A[0].cols() == in.size());
+  Mat work(in.size(), in.size());
+  Vec one(in.size());
+  for(int i = 0; i < A.size(); i ++) {
+    work.setCol(i, A[i] * in);
+    one[i] = T(1);
+  }
+  auto f(work.solve(one));
   return f /= sqrt(f.dot(f));
 }
 
-template <typename T> typename Decompose<T>::Mat Decompose<T>::complementMat(const Vec& in) const {
-  assert(A.cols() == in.size() && bA.size() == in.size());
-  Mat B(A.rows(), A.cols());
-#if defined(_OPENMP)
-#pragma omp parallel for schedule(static, 1)
-#endif
-  for(int i = 0; i < bA.size(); i ++)
-    B.setCol(i, bA[i] * in);
-  return B;
+template <typename T> typename Decompose<T>::Vec Decompose<T>::freq(const Vec& mother, const Vec& in) const {
+  assert(mother.size() && A.size() == mother.size() &&
+         A[0].cols() == mother.size() && mother.size() == in.size());
+  Mat work(in.size(), in.size());
+  for(int i = 0; i < mother.size(); i ++)
+    work.setCol(i, A[i] * mother);
+  return work.solve(in);
+}
+
+template <typename T> typename Decompose<T>::Vec Decompose<T>::synth(const Vec& mother, const Vec& in) const {
+  assert(mother.size() && A.size() == mother.size() &&
+         A[0].cols() == mother.size() && mother.size() == in.size());
+  Vec res(mother.size());
+  for(int i = 0; i < res.size(); i ++)
+    res[i] = T(0);
+  for(int i = 0; i < A.size(); i ++)
+    res += A[i] * mother * in[i];
+  return res;
 }
 
 #define _DECOMPOSE_
