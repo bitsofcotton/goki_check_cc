989e861c19104b25db54e5007193b6ff116d0b0f
bitsofcotton <22259589+bitsofcotton@users.noreply.github.com>
2021-12-31 01:17:03 +0900

    integraw, diffraw but accuracy is severe on pnm.

--- test.py
+++ test.py
@@ -175,7 +175,7 @@ else:
         subprocess.call([argv[1], "sharpen", root + "-sharpen.ppm", root + "-sharpen-work.ppm", str(pixels)])
         subprocess.call(["convert", root + "-sharpen-work.ppm", "-resize", "50%", "-compress", "none", root + "-sharpen.ppm"])
       subprocess.call(["convert", root + ".ppm", root + "-sharpen.ppm", "-compose", "minus", "-composite", "-negate", "-equalize", root + "-sharpen-minus-equalize.png"])
-    elif(argv[2] == "bump" or argv[2] == "pextend" or argv[2] == "represent" or argv[2] == "collect" or argv[2] == "flarge" or argv[2] == "blink"):
+    elif(argv[2] == "bump" or argv[2] == "pextend" or argv[2] == "represent" or argv[2] == "collect" or argv[2] == "flarge" or argv[2] == "blink" or argv[2] == "integraw" or argv[2] == "diffraw"):
       subprocess.call([argv[1], argv[2], root + ".ppm", root + "-" + argv[2] + ".ppm", str(pixels), str(rot)])
     elif(argv[2] == "obj"):
       subprocess.call([argv[1], "obj", str(pixels), "1", root + "-bump.ppm", root + ".obj"])
--- tools.cc
+++ tools.cc
@@ -66,6 +66,8 @@ int main(int argc, const char* argv[]) {
   }
   if(strcmp(argv[1], "collect") == 0 ||
      strcmp(argv[1], "integ") == 0 ||
+     strcmp(argv[1], "diffraw") == 0 ||
+     strcmp(argv[1], "integraw") == 0 ||
      strcmp(argv[1], "enlarge") == 0 ||
      strcmp(argv[1], "flarge") == 0 ||
      strcmp(argv[1], "pextend") == 0 ||
@@ -91,6 +93,12 @@ int main(int argc, const char* argv[]) {
     else if(strcmp(argv[1], "integ") == 0)
       for(int i = 0; i < data.size(); i ++)
         data[i] = filter<num_t>(data[i], INTEG_BOTH, recur, rot);
+    else if(strcmp(argv[1], "diffraw") == 0)
+      for(int i = 0; i < data.size(); i ++)
+        data[i] = diff<num_t>(data[i].rows()) * data[i] * diff<num_t>(data[i].cols());
+    else if(strcmp(argv[1], "integraw") == 0)
+      for(int i = 0; i < data.size(); i ++)
+        data[i] = diff<num_t>(- data[i].rows()) * data[i] * diff<num_t>(- data[i].cols());
     else if(strcmp(argv[1], "enlarge") == 0)
       for(int i = 0; i < data.size(); i ++)
         data[i] = filter<num_t>(filter<num_t>(data[i], ENLARGE_BOTH, recur, rot), CLIP);
