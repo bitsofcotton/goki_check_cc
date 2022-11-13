82cab6f61260dba61a8cb39eaba24188b97cf695
bitsofcotton <22259589+bitsofcotton@users.noreply.github.com>
2022-11-13 22:56:49 +0900

    halfen z-axis.
    They have been artificial before we halfen this.

--- goki.hh
+++ goki.hh
@@ -1449,7 +1449,7 @@ template <typename T> SimpleMatrix<T> getTiltAfterBump(const SimpleMatrix<T>& in
 template <typename T> vector<SimpleVector<T> > getTileVec(const SimpleMatrix<T>& in, const int& vbox = 1) {
   vector<SimpleVector<T> > geoms;
   geoms.reserve((in.rows() / vbox + 1) * (in.cols() / vbox + 1));
-  const auto diag(sqrt(T(in.rows() * in.rows() + in.cols() * in.cols())));
+  const auto diag(sqrt(T(in.rows() * in.rows() + in.cols() * in.cols())) / T(int(2)));
   for(int i = 0; i < in.rows() / vbox + 1; i ++)
     for(int j = 0; j < in.cols() / vbox + 1; j ++) {
       if(in.rows() < (i + 1) * vbox ||
@@ -1486,7 +1486,7 @@ template <typename T> vector<SimpleVector<T> > getHesseVec(const SimpleMatrix<T>
   const auto xx(in * diff<T>(in.cols()).transpose() * diff<T>(in.cols()).transpose());
   const auto xy(diff<T>(in.rows()) * in * diff<T>(in.cols()).transpose());
   const auto yy(diff<T>(in.rows()) * diff<T>(in.rows()) * in);
-  const auto diag(sqrt(T(int(in.rows() * in.rows() + in.cols() * in.cols()))));
+  const auto diag(sqrt(T(int(in.rows() * in.rows() + in.cols() * in.cols()))) / T(int(2)));
   vector<pair<T, pair<int, int> > > score;
   score.reserve(in.rows() * in.cols());
   for(int i = 0; i < in.rows(); i ++)
