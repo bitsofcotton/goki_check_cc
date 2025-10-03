6610f23a44d186ad66a2554a9f746895f7c35c89
bitsofcotton <22259589+bitsofcotton@users.noreply.github.com>
2025-10-03 20:35:58 +0900

    merge latest lieonn. add blurpq.

--- test.py
+++ test.py
@@ -143,9 +143,8 @@ else:
         subprocess.call(["mogrify", "-compress", "none", "-blur", "4x4+3", root + "-" + argv[2] + ".ppm"])
     elif(argv[2] == "blurpq"):
       # N.B. estimate aleph_(aleph_omega) bucket's 2/3 value (1 / 2^sqrt(11) as a majority logic with insurance repeat).
-      subprocess.call(["convert", line, "-blur", "10x10+6", root + "-" + argv[2] + ".png"])
+      subprocess.call(["convert", line, "-blur", "11x11+6", root + "-" + argv[2] + ".png"])
       subprocess.call(["convert", line, "-negate", "-blur", "10x10+3", root + "-" + argv[2] + "-neg.png"])
-      
     elif(argv[2] == "obj"):
       subprocess.call([argv[1], argv[2], root + "-bump.ppm", root + ".obj"])
     elif(argv[2] == "jps"):
